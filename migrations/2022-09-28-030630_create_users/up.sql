969fc9ba2a5f9b70c7b314249ccf4148a74ae503
Kyosuke Awata <kyou0209@gmail.com>
2022-09-28 12:12:53 +0900

    usersテーブル追加

---
+++ migrations/2022-09-28-030630_create_users/up.sql
@@ -0,0 +1,8 @@
+-- Your SQL goes here
+
+CREATE TABLE users (
+  id VARCHAR PRIMARY KEY,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL,
+  age SMALLINT NOT NULL
+)
--- src/schema.rs
+++ src/schema.rs
@@ -8,3 +8,17 @@ diesel::table! {
         published -> Bool,
     }
 }
+
+diesel::table! {
+    users (id) {
+        id -> Varchar,
+        first_name -> Varchar,
+        last_name -> Varchar,
+        age -> Int2,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    posts,
+    users,
+);
